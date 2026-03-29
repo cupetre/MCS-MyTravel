eba0be1a2796497175f8884b5cde4eb567eb6855
dacupetre <cupetre_kiko@yahoo.com>
2026-03-30 01:25:37 +0200

    starting of project , working with wpf for design and UI look

---
+++ MCS_Software/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MCS_Software
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+                
+        }
+
+        private void ShowNoClientSelectedView()
+        {
+            NoClientSelectedView.Visibility = Visibility.Visible;
+            SelectedClientView.Visibility = Visibility.Collapsed;
+            AddNewClientView.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowSelectedClientView()
+        {
+            NoClientSelectedView.Visibility = Visibility.Collapsed;
+            SelectedClientView.Visibility = Visibility.Visible;
+            AddNewClientView.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowAddNewClientView()
+        {
+            NoClientSelectedView.Visibility = Visibility.Collapsed;
+            SelectedClientView.Visibility = Visibility.Collapsed;
+            AddNewClientView.Visibility = Visibility.Visible;
+        }
+
+        private void AddNewClient_Click(object sender, RoutedEventArgs e)
+        {
+            ShowAddNewClientView();
+        }
+
+        private void CancelAddNewClient_Click(object sender, RoutedEventArgs e)
+        {
+            ShowNoClientSelectedView();
+        }
+    }
+}
