22cad6922ee7724066fcb8735b0b5b364db63830
dacupetre <cupetre_kiko@yahoo.com>
2026-04-11 12:15:16 +0200

    adding database migrations

--- MCS_Software/App.xaml.cs
+++ MCS_Software/App.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MCS_Software.Data;
+using System.IO;
 using System.Windows;
 
 namespace MCS_Software
@@ -13,5 +15,49 @@ namespace MCS_Software
     /// </summary>
     public partial class App : Application
     {
+        public static IHost AppHost { get; private set; } = null;
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+
+                    services.AddDbContext<AppDbContext>(options =>
+                        options.UseNpgsql(connectionString));
+
+                    services.AddTransient<MainWindow>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost.StartAsync();
+
+            using (var scope = AppHost.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
+
+            var mainWindow = AppHost.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost.StopAsync();
+            AppHost.Dispose();
+            base.OnExit(e);
+        }
     }
 }
